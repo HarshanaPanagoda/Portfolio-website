2391fd2b77f3ea2f5bb2abcb860325a04b239335
Harshana Panagoda <w1998772@my.westminster.ac.uk>
2024-04-24 17:23:02 +0530

    commit

--- script.js
+++ script.js
@@ -4,3 +4,65 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+/*animated text */
+
+const txts = document.querySelector(".animate-text").children,
+  txtsLen = txts.length;
+let index = 0;
+const textInTimer = 3000,
+  textOutTimer = 2800;
+
+function animateText() {
+  for (let i = 0; i < txtsLen; i++) {
+    txts[i].classList.remove("text-in", "text-out");
+  }
+  txts[index].classList.add("text-in");
+
+  setTimeout(function () {
+    txts[index].classList.add("text-out");
+  }, textOutTimer);
+
+  setTimeout(function () {
+    if (index == txtsLen - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    animateText();
+  }, textInTimer);
+}
+
+window.onload = animateText;
+
+/*scroll button behavior */
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.scrollY > 300) {
+    // Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  } else {
+    // Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function () {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", BackToTop);
+function BackToTop() {
+  window.scrollTo(0, 0);
+}
+
